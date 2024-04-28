9dc15339713fe5f27b96cc70d05f1e1cfb4b8a24
infoarifk <inbox.arifkhan@gmail.com>
2024-04-28 13:35:14 +0600

    function created

--- main.sh
+++ main.sh
@@ -1,6 +1,66 @@
 #!/bin/bash
 
+declare -A accounts
+
+create_user() {
+
+	read -p "Enter your username: " username
+
+	if (( ${#accounts[$username]} !=0 ))
+	then
+	echo "
+	-------------------------------------------------
+	Username already exist. Please choose another one
+	-------------------------------------------------
+	"
+	else
+	read -s -p "Enter Password: " password
+
+	accounts[$username]=$password
+
+	echo "
+	----------------------------------------------
+	Account created successfully. Now Login please
+	----------------------------------------------
+	"
+	return 1
+	fi
+
+
+}
+
+
+user_login() {
+
+	echo "Enter your username: "
+	read username
+	if (( ${#accounts[$username]} !=0 ))
+	then
+	echo "Enter password"
+	read -s password
+		if (( ${accounts[username]} == $password ))
+		then
+		echo "Login successful. Welcome, $username"
+		return 0
+		else
+		echo "Incorrect password. Please try again"
+		return 1
+		fi
+	else
+	echo "Username not found. Please create an account"
+	return 1
+	fi
+
+
+
+
+
+
+
+}
 echo "==========WELCOME TO XYZ BANK==========="
+
+
 #front page
 
 while true
@@ -12,10 +72,11 @@ read -p "Choose an option: " option
 
 if (($option == 1))
 then
-	echo "Login"
+	user_login
 elif (($option == 2))
 then
-	echo "Create Account"
+	create_user
+
 elif (($option == 3))
 then
 	echo "Exiting.."
