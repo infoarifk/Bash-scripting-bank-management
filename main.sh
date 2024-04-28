c6611027cae105ca28ea5f7472c2e104f5873844
infoarifk <inbox.arifkhan@gmail.com>
2024-04-28 11:21:18 +0600

    working on

--- main.sh
+++ main.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 echo "==========WELCOME TO XYZ BANK==========="
+#front page
+
+while true
+do
+echo "1. Login"
+echo "2. Create Account"
+echo "3. Exit"
+read -p "Choose an option: " option
+
+if (($option == 1))
+then
+	echo "Login"
+elif (($option == 2))
+then
+	echo "Create Account"
+elif (($option == 3))
+then
+	echo "Exiting.."
+	exit
+else
+	echo "Invalid option. Please try again"
+fi
+done
