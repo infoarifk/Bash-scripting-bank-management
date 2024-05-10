08df3e066ee2cded6ab7143704c670647965de0a
infoarifk <inbox.arifkhan@gmail.com>
2024-05-11 01:46:10 +0600

    project done

--- main.sh
+++ main.sh
@@ -4,10 +4,10 @@
 usernames=()
 passwords=()
 
-# Array to store corresponding account balances
+# Array to store account balances
 balances=()
 
-# Function to validate login credentials
+# Login function
 login() {
     read -p "Enter username: " username
     read -sp "Enter password: " password
@@ -18,8 +18,11 @@ login() {
 
     for ((i=0; i<${#usernames[@]}; i++)); do
         if [[ "${usernames[$i]}" == "$username" && "${passwords[$i]}" == "$password" ]]; then
+	    clear
             echo "Login successful. Welcome ${usernames[$i]}"
             login_success=true
+	    echo "=========================================="
+	    echo
             main_menu $i
             break
         fi
@@ -40,7 +43,7 @@ create_account() {
     usernames+=("$new_username")
     passwords+=("$new_password")
     balances+=(0)
-
+    clear
     echo "Account created successfully for $new_username"
     main
 }
@@ -48,8 +51,11 @@ create_account() {
 # Function to check account details
 account_details() {
     local index=$1
+    echo "Your Account Details:"
+    echo
     echo "Username: ${usernames[$index]}"
     echo "Current Balance: ${balances[$index]}"
+    echo "===================================="
 }
 
 # Function to deposit money
@@ -69,7 +75,8 @@ withdraw() {
 }
 # Function to display the main menu
 main_menu() {
-    echo "Welcome ${usernames[$1]} to the Bank Management System"
+
+    echo
     echo "1. Account Details"
     echo "2. Deposit"
     echo "3. Withdraw"
@@ -78,15 +85,19 @@ main_menu() {
     read -p "Enter your choice: " choice
 
     if [[ $choice == 1 ]]; then
+	clear
         account_details $1
     elif [[ $choice == 2 ]]; then
+	clear
         read -p "Enter amount to deposit: " amount
         deposit $1 $amount
     elif [[ $choice == 3 ]]; then
+	clear
         read -p "Enter amount to withdraw: " amount
         withdraw $1 $amount
     elif [[ $choice == 4 ]]; then
         echo "Logout Successful"
+    	clear
         main
     else
         echo "Invalid choice. Please enter a number from 1 to 4."
@@ -97,7 +108,10 @@ main_menu() {
 
 # Main function
 main() {
-    echo "Welcome to the Bank Management System"
+    echo "-------------------"
+    echo "WELCOME TO XYZ BANK"
+    echo "-------------------"
+    echo
     echo "1. Login"
     echo "2. Create Account"
     echo "3. Exit"
@@ -117,5 +131,8 @@ main() {
     fi
 }
 
+
 # Run the main function
+clear
+
 main
